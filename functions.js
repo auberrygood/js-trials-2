943df40788fae0c416459ae9568794a5120f0c9a
Aubrey Sanders <aubrey.sandrs@gmail.com>
2021-04-23 04:08:19 +0000

    completed functions.js functions

--- functions.js
+++ functions.js
@@ -3,14 +3,35 @@
 
 // 1. isHometown
 
-// Define your function here
+function isHometown(town){
+    return town === 'San Francisco';
+}
 
 
 // 2. getFullName
 
-// Define your function here
-
+function getFullName(first_name, last_name) {
+  return `${first_name} ${last_name}`;
+}
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(base_price, state, tax=0.05) {
+    const subtotal = base_price * (1 + tax);
+    let fee = 0;
+    if (state === 'CA') {   
+        fee = 0.03 * subtotal;
+    }
+    else if (state === 'PA') {
+        fee = 2;
+    }
+    else if (state === 'MA') {
+        if (base_price <= 100){
+            fee = 1;
+        }
+        else{
+            fee =3;
+        }
+    }
+    return subtotal + fee;
+}
